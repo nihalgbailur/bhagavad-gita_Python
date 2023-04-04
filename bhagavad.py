8c0a6da727c8ddb5e2cb3b8713929b015486890a
NIHAL G BAILUR <nihalgbailur@NIHALs-MacBook-Air.local>
2023-04-04 22:24:24 +0530

    added another quote

--- bhagavad.py
+++ bhagavad.py
@@ -4,7 +4,8 @@ quotes = [("You have the right to work, but never to the fruit of the work.", "C
           ("One who has control over the mind is tranquil in heat and cold, in pleasure and pain, and in honor and dishonor.", "Chapter 6, Verse 7"),
           ("The self-controlled soul, who moves amongst sense objects, free from either attachment or repulsion, he wins eternal Peace.", "Chapter 2, Verse 64"),
           ("The embodied soul is eternal in existence, indestructible, and infinite, only the material body is factually perishable.", "Chapter 2, Verse 18"),
-          ("The wise see that there is action in the midst of inaction and inaction in the midst of action.", "Chapter 4, Verse 18")]
+          ("The wise see that there is action in the midst of inaction and inaction in the midst of action.", "Chapter 4, Verse 18"),
+          ("The awakened sages call a person wise when all his undertakings are free from anxiety about results."," (Chapter 4, Verse 19)"),]
 
 random_quote = random.choice(quotes)
 print(random_quote[0])
